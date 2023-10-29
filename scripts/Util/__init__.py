f4c46e27071c54af12176edabfd67abd74cdaa48
kriap139 <kripin41@gmail.com>
2023-10-29 11:10:37 +0100

    Small refactoring

--- scripts/Util/__init__.py
+++
@@ -1,8 +0,0 @@
-from Util.dataset import (
-    Dataset, DatasetInfo, extract_labels, get_dataset_info, 
-    get_train_dataset, get_test_dataset, get_dataset_labels
-) 
-
-from Util.io_util import (
-    arff_to_csv, data_dir, load_arff
-)
