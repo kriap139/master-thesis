d09ba03cc592a58835ce0e556fbc82059094be8a
kriap139 <kripin41@gmail.com>
2023-11-20 01:10:02 +0100

    added test jobs

--- tools/search.py
+++ tools/search.py
@@ -1,9 +1,14 @@
 from Util import Dataset, Builtin, Task, data_dir, Integer, Real, Categorical
 import lightgbm as lgb
 import logging
-from benchmark import BaseSearch, RandomSearch, RepeatedStratifiedKFold, RepeatedKFold, KFold, StratifiedKFold
+from benchmark import BaseSearch, RepeatedStratifiedKFold, RepeatedKFold, KFold, StratifiedKFold
 from typing import Union
+import benchmark
 import psutil
+import argparse
+
+MAX_SEARCH_JOBS = 4
+CPU_CORES = psutil.cpu_count(logical=False)
 
 OBJECTIVES = {
         Task.BINARY: "binary",
@@ -27,21 +32,37 @@ def get_sklearn_model(dataset: Dataset, **params) -> Union[lgb.LGBMClassifier, l
         elif dataset.task == Task.REGRESSION:
             return lgb.LGBMRegressor(**params)
 
+def calc_n_lgb_jobs(n_search_jobs: int, max_lgb_jobs: int) -> int:
+    n_jobs = int(float(CPU_CORES) / search_n_jobs)
+    return min(min(n_jobs, CPU_CORES), max_lgb_jobs)
+
+def build_cli() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="...")
+    parser.add_argument("--method", 
+        choices=("RandomSearch", "SeqUDSearch"),
+        type=str,
+        default=""
+    )
+    parser.add_argument("--n-jobs", type=int, default=MAX_SEARCH_JOBS)
+    parser.add_argument("--max-lgb-jobs", type=int, default=CPU_CORES)
+    
+    return parser.parse_args()
+
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.DEBUG)
+    args = build_cli()
 
-    SEARCH_N_JOBS = 4
-    cpu_cores = psutil.cpu_count(logical=False)
-    n_jobs=int(float(cpu_cores) / SEARCH_N_JOBS) if SEARCH_N_JOBS is not None else None
-    print(f"CPU Cores: {cpu_cores}, Logical Cores: {psutil.cpu_count(logical=True)}, lgb_n_jobs={n_jobs}, search_n_jobs={SEARCH_N_JOBS}")
+    search_n_jobs = min(args.n_jobs, MAX_SEARCH_JOBS)
+    n_jobs= calc_n_lgb_jobs(search_n_jobs, args.max_lgb_jobs)
+    print(f"CPU Cores: {CPU_CORES}, Logical Cores: {psutil.cpu_count(logical=True)}, lgb_n_jobs={n_jobs}, search_n_jobs={search_n_jobs}")
 
     search_space = dict(
-        n_estimators=Integer(1, 500, name="n_estimators"),
+        n_estimators=Integer(1, 500, name="n_estimators", prior="log-uniform"),
         learning_rate=Real(0.0001, 1.0, name="learning_rate", prior="log-uniform"),
         max_depth=Integer(0, 30, name="max_depth"),
-        num_leaves=Integer(10, 300, name="num_leaves"),
+        num_leaves=Integer(10, 300, name="num_leaves", prior="log-uniform"),
         min_data_in_leaf=Integer(0, 30, name="min_data_in_leaf"),
-        feature_fraction=Real(0.1, 1.0, name="feature_fraction")
+        feature_fraction=Real(0.1, 1.0, name="feature_fraction", prior="log-uniform")
     )
 
     dataset = Dataset(Builtin.OKCUPID_STEM).load()
@@ -56,12 +77,13 @@ if __name__ == "__main__":
         categorical_feature=dataset.cat_features,
     )
 
-    tuner = RandomSearch
+    tuner = getattr(benchmark, args.method)
     save_dir = data_dir(f"test_results/{tuner.__name__}[{dataset.name}]")
     model = get_sklearn_model(dataset, verbose=-1, n_jobs=n_jobs)
 
     cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=10, random_state=10)
     tuner = tuner(model=model, train_data=dataset, test_data=None, n_iter=100, 
-                  n_jobs=SEARCH_N_JOBS, cv=cv, inner_cv=None, scoring=None, save_dir=save_dir)
+                  n_jobs=search_n_jobs, cv=cv, inner_cv=None, scoring=None, save_dir=save_dir)
 
+    print(f"Results sved to: {tuner._save_dir}")
     tuner.search(search_space, fixed_params)
