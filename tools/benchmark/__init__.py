01725af1c9ec2a06ebbe37d65947fae7f4d7ac84
kriap139 <kripin41@gmail.com>
2024-02-19 05:57:48 +0100

    Bugfixing

--- tools/benchmark/__init__.py
+++ tools/benchmark/__init__.py
@@ -1,5 +1,4 @@
 from .base_search import BaseSearch, KFold, StratifiedKFold, RepeatedKFold, RepeatedStratifiedKFold
 from .sklearn_search import RandomSearch, GridSearch
-from .sequd_search import SeqUDSearch
+from .sequd_search import SeqUDSearch, AdjustedSeqUDSearch
 from .optuna_search import OptunaSearch
-from .adjusted_sequd_search import AdjustedSeqUDSearch
