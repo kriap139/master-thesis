fb79e96339b4efca262d86f4b105efc614c60682
kriap139 <kripin41@gmail.com>
2024-04-25 01:08:27 +0200

    better results printing

--- src/calc_metrics.py
+++ src/calc_metrics.py
@@ -449,7 +449,7 @@ def print_untesed_kspace_combos(
             if len(tested_params) > 0:
                 untested = filter(lambda params: not any(params == p for p in tested_params), kspace_params)
                 sub_strings = '\n      ' + f'\n      '.join([str(d) for d in untested])
-                strings.append(f"   {method}({file_path}):{sub_strings}")
+                strings.append(f"   {method}(n_tested={len(tested_params)},n_kspace={len(kspace_params)}):{sub_strings}")
 
         print(f"{dataset}: \n" + "\n".join(strings) + '\n')
         strings.clear()
