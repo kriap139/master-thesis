2c6fa186fd45c0961b5e84b598d40c4416038c2b
kriap139 <kripin41@gmail.com>
2024-05-04 14:03:49 +0200

    Made repeats modifiable in slur_job script and updated method slurm job files

--- scripts/slurm_job_tmpl.sh
+++ scripts/slurm_job_tmpl.sh
@@ -1,38 +1,82 @@
 #!/bin/bash
 
 max_lgb_jobs=10
-n_repeats=3
 n_folds=5
 inner_n_folds=5
 inner_shuffle=1
 inner_random_state=9
+default_repeats=3
 large_datasets=('higgs' 'rcv1')
 
-method="$1"
-datasets=()
-params_file="$4"
+repeats=()
 param_indexes=()
+datasets=()
+params_file="''"
+search_space="all"
 
-search_space="$2"
-if [ "$2" == "all" ]
+# Get the options
+while getopts ":f:i:d:s:m:r:" option; do
+   case $option in
+      d) # datasets
+         for a in $OPTARG; do datasets+=("$a") ; done
+         last_dataset=${datasets[$(( ${#datasets[*]} - 1 ))]};;
+      
+      i) # param indexes
+         for a in $OPTARG; do param_indexes+=("$a") ; done;;
+      
+      r) # Repeats
+         for a in $OPTARG; do repeats+=("$a") ; done;;
+      
+      f) # params file
+         params_file="$OPTARG";;
+      
+      s) # Restrict search space
+         search_space="$OPTARG";;
+      
+      m) # Search Method
+         method="$OPTARG";;
+
+     \?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done  
+
+if [ -z "$method" ]
+then
+    echo "Search Method not specified!"
+    exit 1
+fi
+
+if [ ${#repeats[@]} -eq 0 ]
+then
+    echo "repeats array is empty, using defaults"
+    for dataset in "${datasets[@]}"
+    do
+        if [[ ${large_datasets[*]} =~ $dataset ]]
+        then
+            repeats+=("0")
+        else
+            repeats+=("$default_repeats")
+        fi
+    done
+fi 
+
+if [ "$search_space" == "all" ]
 then
     echo "search space unretricted!"
     unset search_space
 else
-    echo "restricting search space to: [$2]"
+    echo "restricting search space to: [$search_space]"
 fi
-
-for i in $3; do datasets+=("$i") ; done
-for i in $5; do param_indexes+=("$i") ; done
-
-last_dataset=${datasets[$(( ${#datasets[*]} - 1 ))]}
-echo "datasets=[${datasets[*]}], last_dataset=${last_dataset[*]}, params_file=$params_file, param_indexes=[${param_indexes[*]}]"
 
 if [ ! -f "$params_file" ]
 then
     echo "params file dosen't exists, Params indexes reset!"
     param_indexes=()
 fi
+
+echo "datasets=[${datasets[*]}], last_dataset=${last_dataset[*]}, repeats=[${repeats[*]}], params_file=$params_file, param_indexes=[${param_indexes[*]}]"
 
 set_copy() {
     unset move
@@ -45,9 +89,9 @@ set_move() {
 }
 
 run_search() {
-    #echo "method: $method, dataset: $dataset, inner_shuffle: ${inner_shuffle:+--inner-shuffle}, params: ${params:+--params "$params"}"
-    #echo "move: ${move:+--move-slurm-logs}, copy_logs: ${copy_logs:+--copy-new-slurm-log-lines}"
-    #exit 0
+    echo "method: $method, dataset: $dataset, inner_shuffle: ${inner_shuffle:+--inner-shuffle}, params: ${params:+--params "$params"}"
+    echo "move: ${move:+--move-slurm-logs}, copy_logs: ${copy_logs:+--copy-new-slurm-log-lines}"
+    exit 0
     python3 ./src/search.py                   \
     ${move:+--move-slurm-logs}                  \
     ${copy_logs:+--copy-new-slurm-log-lines}    \
@@ -95,14 +139,10 @@ run_search_no_params() {
     run_search
 }
 
-for dataset in "${datasets[@]}"
+for dataset_idx in "${!datasets[@]}"
 do  
-    _repeats=$n_repeats
-
-    if [[ ${large_datasets[*]} =~ $dataset ]]
-    then
-        n_repeats=0
-    fi
+    dataset=${datasets[$dataset_idx]}
+    n_repeats=${repeats[$dataset_idx]}
     
     if [ ${#param_indexes[@]} -eq 0 ]; 
     then
@@ -112,5 +152,4 @@ do
         echo "Running search with params"
         run_search_with_params
     fi
-    n_repeats=$_repeats
 done
