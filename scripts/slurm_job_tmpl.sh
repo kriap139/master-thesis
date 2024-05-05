0fcc806d06bdad49fa746df1319ae4253125b05a
kriap139 <kripin41@gmail.com>
2024-05-05 16:19:44 +0200

    search_space now infered if params_file is passed!

--- scripts/slurm_job_tmpl.sh
+++ scripts/slurm_job_tmpl.sh
@@ -5,14 +5,26 @@ n_folds=5
 inner_n_folds=5
 inner_shuffle=1
 inner_random_state=9
+
 default_repeats=3
 large_datasets=('higgs' 'rcv1')
 
 repeats=()
+search_spaces=()
 param_indexes=()
 datasets=()
 params_file="''"
-search_space="all"
+
+print_exit() {
+    echo "$1"
+    exit 1
+}
+
+check_exit() {
+    if [ $? -ne 0 ]; then
+        print_exit "$1"
+    fi
+}
 
 # Get the options
 while getopts ":f:i:d:s:m:r:" option; do
@@ -31,7 +43,7 @@ while getopts ":f:i:d:s:m:r:" option; do
          params_file="$OPTARG";;
       
       s) # Restrict search space
-         search_space="$OPTARG";;
+         for a in $OPTARG; do search_spaces+=("$a") ; done;;
       
       m) # Search Method
          method="$OPTARG";;
@@ -42,11 +54,15 @@ while getopts ":f:i:d:s:m:r:" option; do
    esac
 done  
 
-if [ -z "$method" ]
+
+if [ -z "$method" ]; then print_exit "Search Method not specified!"; fi
+
+if [ ! -f "$params_file" ]
 then
-    echo "Search Method not specified!"
-    exit 1
+    echo "params file dosen't exists, Params indexes reset!"
+    param_indexes=()
 fi
+
 
 if [ ${#repeats[@]} -eq 0 ]
 then
@@ -60,20 +76,42 @@ then
             repeats+=("$default_repeats")
         fi
     done
+elif [ ${#repeats[@]} -eq 1 ] && [ ${#datasets[@]} -gt 1 ]; then
+    n_repeats=${repeats[0]}
+    repeats=() && for a in "${datasets[@]}"; do repeats+=("$n_repeats") ; done
+    unset n_repeats
+    echo "Expanding single repeats, to all datsets: ${repeats[*]}"
+elif [ ${#repeats[@]} -gt 1 ] && [ ${#datasets[@]} -ne ${#repeats[@]} ]; then
+    print_exit "Number of given repeats (${#repeats[@]}) is smaller than the number of datasets(${#datasets[@]})!"
 fi 
 
-if [ "$search_space" == "all" ]
-then
-    echo "search space unretricted!"
-    unset search_space
-else
-    echo "restricting search space to: [$search_space]"
-fi
 
-if [ ! -f "$params_file" ]
-then
-    echo "params file dosen't exists, Params indexes reset!"
-    param_indexes=()
+
+if [ ${#search_spaces[@]} -eq 0 ] && [ ${#param_indexes[@]} -eq 0 ]; then
+    for dataset in "${datasets[@]}"; do search_spaces+=("all") ; done
+    echo "Search space not set, and no params indexes present. Unrestrincting search spaces: ${search_spaces[*]}"
+
+elif [ ${#search_spaces[@]} -eq 1 ] && [ ${#param_indexes[@]} -eq 0 ] && [ ${#datasets[@]} -gt 1 ]; then
+    search_space=${search_spaces[0]}
+    search_spaces=()
+    for a in "${datasets[@]}"; do search_spaces+=("$search_space") ; done
+    unset search_space
+    echo "Expanding single search_space, to all datsets: ${search_spaces[*]}"
+
+elif [ ${#search_spaces[@]} -eq 0 ] && [ ${#param_indexes[@]} -gt 0 ]; then
+    spaces="$(python3 src/Util/params_file_infer_search_space.py "$params_file" "${param_indexes[@]}" )"
+    check_exit "Failed to infer search spaces: $spaces"
+    search_spaces=() && for a in $spaces; do search_spaces+=("$a"); done
+    echo "infered search space from params file: ${search_spaces[*]}"
+
+elif [ ${#param_indexes[@]} -eq 0 ] && [ ${#search_spaces[@]} -gt 0 ] && [ ${#search_spaces[@]} -ne ${#datasets[@]} ]; then
+    print_exit "Number of given search spaces (${#search_spaces[@]}) is less than the number of datasets (${#datasets[@]})!"
+
+elif [ ${#search_spaces[@]} -gt 0 ] && [ ${#param_indexes[@]} -gt 0 ]; then
+    echo "It is not possible to specify search space and params at the same time, as the search space will be infered from the params!"
+    echo "it is possible to add a search space key to params in the params file instead!. However if stuff like k space parameters-"
+    echo "are already present in the params file, then these will take precedent!"
+    exit 1
 fi
 
 echo "datasets=[${datasets[*]}], last_dataset=${last_dataset[*]}, repeats=[${repeats[*]}], params_file=$params_file, param_indexes=[${param_indexes[*]}]"
@@ -86,6 +124,15 @@ set_copy() {
 set_move() {
      move=1
     unset copy_logs
+}
+
+print_search_space_info() {
+    if [ "$search_space" == "all" ]
+    then
+        echo "search space unretricted!"
+    else
+        echo "restricting search space to: $search_space"
+    fi
 }
 
 run_search() {
@@ -111,7 +158,9 @@ run_search_with_params() {
     for i in "${!param_indexes[@]}"
     do
         params_idx=${param_indexes[$i]}
+        search_space=${search_spaces[$i]}
         params="$(python3 src/Util/params_file_to_cmd.py "$params_file" "$params_idx")"
+        print_search_space_info
         
         # last params index
         if [ $i -eq $((${#param_indexes[@]}-1)) ] && [[ "$dataset" == "$last_dataset" ]]
@@ -127,6 +176,8 @@ run_search_with_params() {
 }
 
 run_search_no_params() {
+    print_search_space_info
+
     # last params index
     if [[ $dataset == "$last_dataset" ]]
     then
@@ -146,6 +197,7 @@ do
     
     if [ ${#param_indexes[@]} -eq 0 ]; 
     then
+        search_space=${search_spaces[$dataset_idx]}
         echo "Running search no params"
         run_search_no_params
     else
---
+++ src/Util/params_file_infer_search_space.py
@@ -0,0 +1,45 @@
+import sys
+import json
+from numbers import Number
+from typing import Union, List
+
+def get_search_space(params: dict) -> str:
+    if 'k' in params:
+        keys = params["k"].keys()
+        return ",".join(keys)
+    elif "search_space" in params:
+        return params["search_space"]
+    else:
+        return "all"
+
+def main(fp: str, indexes: Union[int, List[int]] = None) -> Union[str, dict, list]:
+    with open(fp, mode='r') as f:
+        array: list = json.load(f)
+    if isinstance(indexes, int):
+        indexes = (indexes, )
+    
+    results = []
+    for index in indexes:
+        params: dict = array[index]
+        results.append(get_search_space(params))
+    
+    if len(results) == 1:
+        return results[0]
+    elif len(results) > 1:
+        return " ".join(results)
+        
+        
+if __name__ == "__main__":
+    n_idx = len(sys.argv) - 2
+
+    if n_idx < 0:
+        raise RuntimeError(f"Need arguments FILE INDEXES")
+    elif n_idx == 0:
+        idxes = None
+    elif n_idx == 1:
+        idxes = int(sys.argv[2])
+    else:
+        args = sys.argv[2:]
+        idxes = [int(s) for s in args]
+
+    print(main(sys.argv[1], idxes))
