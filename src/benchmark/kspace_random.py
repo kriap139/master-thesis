8040eb65ba89e97a02489ecbd034c0804fe7d218
kriap139 <kripin41@gmail.com>
2024-10-24 16:57:29 +0200

    Added KSpace random

---
+++ src/benchmark/kspace_random.py
@@ -0,0 +1,31 @@
+from .base_search import BaseSearch, InnerResult
+from Util import Dataset, TY_CV, Integer, Real, Categorical, save_csv, TY_SPACE
+import lightgbm as lgb
+from typing import Callable, Iterable, Dict, Union, Sequence
+from numbers import Number
+import time
+import numpy as np
+import json
+from optuna import Trial
+import pandas as pd
+from sklearn.model_selection import KFold
+from sklearn.base import clone
+from sklearn.utils.estimator_checks import check_estimator
+from kspace import KSpaceStudy, infer_kspace_ver
+from kspace import KSpaceRandomSearch
+        
+class KSpaceRandomSearchV3(BaseSearch):
+    def __init__(self, k:  Union[Number, dict] = None, *args, **kwargs):
+        self.k = k
+        self.x_in_search_space = True
+        self.kspace_ver = 3
+        super().__init__(*args, **kwargs)
+
+    def _create_save_dir(self) -> str:
+        info = dict(kparams=len(self.k.keys())) if isinstance(self.k, dict) else None
+        return super()._create_save_dir(info)
+    
+    def _inner_search(self, search_iter: int, x_train: pd.DataFrame, y_train: pd.DataFrame, search_space: dict, fixed_params: dict) -> InnerResult:
+        search = KSpaceRandomSearch(self._model, search_space, k=self.k, n_iter=self.n_iter, scoring=self.scoring, n_jobs=self.n_jobs, cv=self.inner_cv, refit=self.refit)
+        results = search.fit(x_train, y_train, **fixed_params)
+        return InnerResult(results.best_index_, results.best_params_, results.best_score_, results.cv_results_, results.best_estimator_)
---
+++ src/kspace/k_space_random.py
@@ -0,0 +1,39 @@
+import time
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import cross_val_score, RandomizedSearchCV, ParameterSampler
+from itertools import chain
+from typing import Iterable, Union, Iterable, Callable
+from numbers import Number
+from .k_space import KSpaceV3
+
+
+class KSpaceRandomSearch(RandomizedSearchCV):
+    def __init__(self, model, k_space: TY_SPACE, k:  Union[Number, dict] = None, *args, **kwargs):
+        super().__init__(model, k_space, *args, **kwargs)
+        self.kspace = KSpaceV3(k_space, k, x_in_search_space=True)
+    
+    def _run_search(self, evaluate_candidates):
+        """Search n_iter candidates from param_distributions"""
+        sampled = ParameterSampler(
+            self.param_distributions, self.n_iter, random_state=self.random_state
+        )
+
+        candidates = []
+        for sample in sampled:
+            candidates.append(
+                {param: self.kspace.kmap(param, v, default=v) for param, v in sample.items()}
+            )
+
+        evaluate_candidates(candidates)
+
+        frame = pd.DataFrame(self.cv_results_)
+        for column, values in pd.DataFrame(data).to_dict(orient="list"):
+            frame[column + '_original'] = values
+        self.cv_results_ = frame
+
+
+
+
+        self.cv_results_ = results
+    
