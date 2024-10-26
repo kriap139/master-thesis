e6cbe401e56392076a4ce35cdb996cdb30ef9096
kriap139 <kripin41@gmail.com>
2024-10-27 00:19:10 +0200

    bugfix

--- src/benchmark/ksearch_optuna.py
+++ src/benchmark/ksearch_optuna.py
@@ -20,8 +20,6 @@ import sys
 class KSearchOptuna(BaseSearch):    
     def __init__(self, ksearch_iter: int = 100, resume_dir: str = None, search_method=None, *args, **kwargs):
         self.search_method = search_method
-        super().__init__(*args, **kwargs, save_dir=resume_dir)
-
         self.ksearch_iter = ksearch_iter
         self._passed_kwargs = kwargs
         self._study = create_study(sampler=TPESampler(), direction="maximize")
@@ -43,6 +41,8 @@ class KSearchOptuna(BaseSearch):
         else:
             self._method = KSpaceOptunaSearchV3
 
+        super().__init__(*args, **kwargs, save_dir=resume_dir)
+        
         if 'model' in self._passed_kwargs:
             kwargs.pop('model')
         if resume_dir is not None:
